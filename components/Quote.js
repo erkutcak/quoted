7b85c1e3dec3fd318ebbf899f04eb94d92bdb380
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-07-04 19:30:16 -0400

    bug fixes

--- components/Quote.js
+++ components/Quote.js
@@ -67,9 +67,18 @@ export default function Quote () {
                 <h3 className='text-off-white text-xl font-montserrat font-medium italic'>{`"${quote.title}"`}</h3>
                 <h4 className='text-off-white text-lg font-montserrat font-light text-right italic'>{`-${quote.author}`}</h4>
                 <div className="flex justify-around items-center mt-4">
-                    <h5 className='text-off-white inline-block text-xl text-center'>{quote.likes} Likes</h5>
-                    <button onClick={() => handleLike(quote.id, quote.likes)} className='text-off-white ml-4 mr-4 bg-steel-blue py-2 px-4 rounded'>Like!</button>
-                    <h5 className='text-off-white text-sm font-montserrat font-light text-right italic'>{formattedDate}</h5>
+                <button className="flex items-center justify-start border-none rounded-lg overflow-hidden shadow-md cursor-pointer bg-transparent w-30 h-35" onClick={() => handleLike(quote.id, quote.likes)}>
+                    <span className="w-[60px] h-full flex items-center justify-start gap-2 bg-red-500 pl-[5px] pr-[5px] transition-colors duration-300 hover:bg-red-700 active:bg-red-800">
+                        <svg fill="white" viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"></path>
+                        </svg>
+                        <span className="text-white font-semibold">Like</span>
+                    </span>
+                    <span className="w-10 h-full flex items-center justify-center text-red-500 font-semibold relative bg-white">
+                    {quote.likes}
+                    </span>
+                </button>
+                <h5 className='text-off-white text-sm font-montserrat font-light text-right italic'>{formattedDate}</h5>
                 </div>
             </div>
         )
